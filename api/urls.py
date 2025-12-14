075f9fddd92319f9873c9ffd0386e36a1a8f8974
kollasagar18 <kollasagar03@gmail.com>
2025-12-15 00:28:27 +0530

    api with

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import home,creating_emp
+
+urlpatterns = [
+    path("home",home),
+    path("creating_emp",creating_emp,name="create_emp12")
+]
---
+++ api/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+import json
+file_name="api/static/emps.json"
+
+def read_data():
+    with open(file_name,"r") as f:
+        return json.load(f)
+        
+def write_data(data):
+    with open(file_name,"w") as f:
+        json.dump(data,f)
+def home(request):
+    return render(request, "home.html")
+
+@api_view(["POST"])
+def creating_emp(request):
+    data = request.POST.dict()
+    data_fram=read_data()
+    data_fram["employess"].append(data)
+    write_data(data_fram)
+    # check data in terminal
+    return HttpResponse("Form submitted successfully")
+
+
+
+    
